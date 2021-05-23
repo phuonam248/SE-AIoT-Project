3ff2976e1dc0e89e52775b12cd6dbb8228f1a128
phuonam248 <phuonam248@gmail.com>
2021-05-24 00:07:57 +0700

    upload code

--- mylib/config.py
+++ mylib/config.py
@@ -5,7 +5,7 @@
 # e.g., 'email@gmail.com'
 MAIL = ''
 # Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
-url = ''
+url = 0
 
 # ON/OFF for mail feature. Enter True to turn on the email alert feature.
 ALERT = False
